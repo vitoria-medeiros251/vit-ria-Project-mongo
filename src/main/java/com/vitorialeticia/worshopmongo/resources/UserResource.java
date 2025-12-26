3d3a2df10be97e8c143130c0c3e63bb520e88741
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2025-12-26 12:23:30 -0300

    obtendo um usuario por id

--- src/main/java/com/vitorialeticia/worshopmongo/resources/UserResource.java
+++ src/main/java/com/vitorialeticia/worshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.vitorialeticia.worshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -24,6 +25,13 @@ public class UserResource {
         List<UserDTO> ListDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(ListDto);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO>  findById(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
 
 
 }
--- src/main/java/com/vitorialeticia/worshopmongo/services/UserService.java
+++ src/main/java/com/vitorialeticia/worshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.vitorialeticia.worshopmongo.services;
 
 import com.vitorialeticia.worshopmongo.domain.User;
 import com.vitorialeticia.worshopmongo.repository.UserRepository;
+import com.vitorialeticia.worshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
        return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
